221e6a5126ad57039a16a9865d90c9c5c4cd9973
Gavin Kasdorf <me@gkasdorf.com>
2023-06-11 00:02:21 -0800

    store login token

--- lemmy/LemmyInstance.ts
+++ lemmy/LemmyInstance.ts
@@ -2,13 +2,17 @@ import ILemmyServer from "./types/ILemmyServer";
 import {LemmyHttp} from "lemmy-js-client";
 
 export let lemmyInstance: LemmyHttp | null = null;
+export let lemmyAuthToken: string | undefined = undefined;
 
 export const initialize = async (server: ILemmyServer) => {
     lemmyInstance = new LemmyHttp(`https://${server.server}`);
-    await lemmyInstance.login({
+
+    const res = await lemmyInstance.login({
         username_or_email: server.username,
         password: server.password
     });
+
+    lemmyAuthToken = res.jwt;
 };
 
 export const isInitialized = () => lemmyInstance !== null;
