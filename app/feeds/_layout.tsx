b5098a6ee9e478dbdaf0be1fbe24b99b2556e59d
Gavin Kasdorf <me@gkasdorf.com>
2023-06-11 11:34:28 -0800

    new post screen

--- app/feeds/_layout.tsx
+++ app/feeds/_layout.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import {Stack} from "expo-router";
+import {Stack, useRouter} from "expo-router";
+import {Button} from "react-native";
 
 const FeedsLayout = () => {
+    const router = useRouter();
+
     return (
         <Stack>
             <Stack.Screen
                 name={"index"}
                 options={{
-                    title: "Feeds"
+                    title: "Feeds",
+                    headerRight: () => (
+                        <Button title={"New Post"} onPress={() => router.push("/feeds/newPost")} />
+                    )
                 }}
             />
             <Stack.Screen
@@ -21,6 +27,12 @@ const FeedsLayout = () => {
                 options={{
                     presentation: "modal",
                     title: "New Comment"
+                }}
+            />
+            <Stack.Screen
+                name={"newPost"}
+                options={{
+                    title: "New Post"
                 }}
             />
         </Stack>
