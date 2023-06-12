4fb2aa93360a14a86e69ff92f59cc327085edd88
Gavin Kasdorf <me@gkasdorf.com>
2023-06-11 19:29:21 -0800

    add redux hooks

--- store.ts
+++ store.ts
@@ -1,5 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {postSlice} from "./slices/post/postSlice";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 
 const store = configureStore({
     reducer: {
@@ -9,5 +10,8 @@ const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
