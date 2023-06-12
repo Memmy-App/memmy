073be5be06b6bae4e70af2cbe017633cbbd51516
Gavin Kasdorf <me@gkasdorf.com>
2023-06-12 12:04:07 -0800

    getpost action for refreshing

---
+++ slices/post/postActions.ts
@@ -0,0 +1,17 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {GetPost} from "lemmy-js-client";
+import {lemmyInstance} from "../../lemmy/LemmyInstance";
+import {setPost} from "./postSlice";
+
+export const getPost = createAsyncThunk(
+    "post/getPost",
+    async (data: GetPost, thunkAPI) => {
+        try {
+            const res = await lemmyInstance.getPost(data);
+
+            thunkAPI.dispatch(setPost(res.post_view));
+        } catch(e) {
+            thunkAPI.rejectWithValue(e);
+        }
+    }
+);
