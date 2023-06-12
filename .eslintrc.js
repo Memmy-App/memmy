4aa565a68cb408d13339d5cca2d989d769f5d710
Gavin Kasdorf <me@gkasdorf.com>
2023-06-12 10:35:44 -0800

    switch rule fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,10 @@ module.exports = {
     "rules": {
         "indent": [
             "error",
-            4
+            4,
+            {
+                "SwitchCase": 1
+            }
         ],
         "linebreak-style": [
             "error",
