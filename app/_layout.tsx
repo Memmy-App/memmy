35962391c5f9e2c30a402d7ba50183501acc0350
Gavin Kasdorf <me@gkasdorf.com>
2023-06-12 02:01:02 -0800

    add action sheet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,28 +4,31 @@ import {NativeBaseProvider} from "native-base";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 import store from "../store";
 import {Provider} from "react-redux";
+import {ActionSheetProvider} from "@expo/react-native-action-sheet";
 
 const Layout = () => {
     return (
         <Provider store={store}>
             <GestureHandlerRootView style={{flex: 1}}>
                 <NativeBaseProvider>
-                    <Tabs>
-                        <Tabs.Screen
-                            name={"feeds"}
-                            options={{
-                                headerShown: false,
-                                tabBarLabel: "Feeds",
-                            }}
-                        />
-                        <Tabs.Screen
-                            name={"servers"}
-                            options={{
-                                headerShown: false,
-                                tabBarLabel: "Servers",
-                            }}
-                        />
-                    </Tabs>
+                    <ActionSheetProvider>
+                        <Tabs>
+                            <Tabs.Screen
+                                name={"feeds"}
+                                options={{
+                                    headerShown: false,
+                                    tabBarLabel: "Feeds",
+                                }}
+                            />
+                            <Tabs.Screen
+                                name={"servers"}
+                                options={{
+                                    headerShown: false,
+                                    tabBarLabel: "Servers",
+                                }}
+                            />
+                        </Tabs>
+                    </ActionSheetProvider>
                 </NativeBaseProvider>
             </GestureHandlerRootView>
         </Provider>
