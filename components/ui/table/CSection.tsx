37f0b8699db6363aa15963df8aa8f3d33b230b8d
Gavin Kasdorf <me@gkasdorf.com>
2023-06-15 15:53:23 -0800

    create table components

---
+++ components/ui/table/CSection.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Section} from "react-native-tableview-simple";
+
+interface CSectionProps {
+    props: object,
+    children: React.ReactNode
+}
+
+const CSection = ({props, children}: CSectionProps) => {
+    return (
+        <Section
+            roundedCorners={true}
+            hideSurroundingSeparators={true}
+            {...props}
+        >
+            {children}
+        </Section>
+    );
+};
+
+export default CSection;
