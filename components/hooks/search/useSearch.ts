09929ee14b8fb15e6d245ed4698a743f37262450
Gavin Kasdorf <me@gkasdorf.com>
2023-06-22 21:34:58 -0800

    directly go to user or community

--- components/hooks/search/useSearch.ts
+++ components/hooks/search/useSearch.ts
@@ -1,8 +1,15 @@
-import { ListingType, SearchType } from "lemmy-js-client";
+import { ListingType, PersonView, PostView, SearchType } from "lemmy-js-client";
 import { SetStateAction, useState } from "react";
+import { Alert } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { lemmyAuthToken, lemmyInstance } from "../../../lemmy/LemmyInstance";
 import ILemmySearchResult from "../../../lemmy/types/ILemmySearchResult";
 import { writeToLog } from "../../../helpers/LogHelper";
+import { getBaseUrl } from "../../../helpers/LinkHelper";
+import { getCommunityFullName } from "../../../lemmy/LemmyHelpers";
+import { setPost } from "../../../slices/post/postSlice";
+import { useAppDispatch } from "../../../store";
 
 interface UseSearch {
   query: string;
@@ -15,6 +22,9 @@ interface UseSearch {
 }
 
 const useSearch = (): UseSearch => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  const dispatch = useAppDispatch();
+
   const [query, setQuery] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
@@ -27,6 +37,46 @@ const useSearch = (): UseSearch => {
   ) => {
     if (!query) return;
 
+    if (query.includes("@") && query.split("@").length === 2) {
+      await Alert.alert(
+        "What do you want to do?",
+        `It looks like ${query} may be a community or user. Where do you want to go?`,
+        [
+          {
+            text: "User",
+            onPress: () => {
+              navigation.push("UserProfile", {
+                fullUsername: query,
+              });
+            },
+          },
+          {
+            text: "Community",
+            onPress: () => {
+              navigation.push("Community", {
+                communityName: query.split("@")[0],
+                actorId: query.split("@")[1],
+                communityFullName: query,
+              });
+            },
+          },
+          {
+            text: "Just Search",
+            onPress: () => {
+              runSearch(type, listingType);
+            },
+          },
+        ]
+      );
+    } else {
+      runSearch(type, listingType).then();
+    }
+  };
+
+  const runSearch = async (
+    type: SearchType,
+    listingType: ListingType = "All"
+  ) => {
     try {
       setLoading(true);
       setError(false);
@@ -36,7 +86,7 @@ const useSearch = (): UseSearch => {
         auth: lemmyAuthToken,
         type_: type,
         q: query,
-        limit: 15,
+        limit: searchType === "All" ? 15 : 40,
         page: 1,
         listing_type: listingType,
         sort: "Active",
