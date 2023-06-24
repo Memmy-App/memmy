83021775faac5619bbeef01089b5ca13c578447a
Gavin Kasdorf <me@gkasdorf.com>
2023-06-23 19:49:09 -0800

    swap out

--- components/ui/image/ImageView.tsx
+++ components/ui/image/ImageView.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { ImageDetail } from "@dreamwalk-os/react-native-image-modal";
 import { Dimensions, Share } from "react-native";
 import { useToast } from "native-base";
 import ImageViewFooter from "./ImageViewFooter";
 import downloadAndSaveImage from "../../../helpers/ImageHelper";
+import ImageModal from "./ImageModal";
 
 interface ImageViewProps {
   source: string;
@@ -33,22 +33,15 @@ function ImageView({ source, setIsOpen, isOpen }: ImageViewProps) {
   };
 
   return (
-    <ImageDetail
+    <ImageModal
+      source={source}
+      onRequestClose={() => setIsOpen(false)}
       isOpen={isOpen}
-      origin={{
-        x: Dimensions.get("window").width / 2,
-        y: Dimensions.get("window").height / 2,
-        width: 100,
-        height: 100,
-      }}
-      source={{
-        uri: source,
-      }}
-      onClose={onClose}
-      swipeToDismiss
-      renderFooter={() => <ImageViewFooter onSave={onSave} onShare={onShare} />}
-      resizeMode="contain"
-      backgroundColor="#000"
+      width={Dimensions.get("screen").width}
+      height={Dimensions.get("screen").height}
+      // renderFooter={() => (
+      //   <ImageViewFooter onShare={onShare} onSave={onSave} />
+      // )}
     />
   );
 }
