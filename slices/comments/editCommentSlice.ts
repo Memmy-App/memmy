26633b670a62e36769c9f5b7b649ea7f719ab403
Gavin Kasdorf <me@gkasdorf.com>
2023-06-29 02:51:55 -0800

    we don't need language ID when we come back

--- slices/comments/editCommentSlice.ts
+++ slices/comments/editCommentSlice.ts
@@ -4,19 +4,11 @@ import { RootState } from "../../store";
 interface EditCommentState {
   commentId: number;
   content: string;
-  languageId: number;
-}
-
-export interface EditCommentAction {
-  commentId: number;
-  content: string;
-  languageId: number;
 }
 
 const initialState: EditCommentState = {
   commentId: 0,
   content: "",
-  languageId: 0,
 };
 
 export const editCommentSlice = createSlice({
@@ -25,11 +17,10 @@ export const editCommentSlice = createSlice({
   reducers: {
     setEditComment: (
       state: EditCommentState,
-      action: PayloadAction<EditCommentAction>
+      action: PayloadAction<EditCommentState>
     ) => {
       state.commentId = action.payload.commentId;
       state.content = action.payload.content;
-      state.languageId = action.payload.languageId;
     },
 
     clearEditComment: () => initialState,
