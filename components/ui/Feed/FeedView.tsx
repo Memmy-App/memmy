043abcd04a89c5f18552439de526ad03e95cb38a
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2023-06-30 16:49:58 -0400

    remove inline components and cleanup

--- components/ui/Feed/FeedView.tsx
+++ components/ui/Feed/FeedView.tsx
@@ -1,64 +1,30 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import { ListingType, PostView, SortType } from "lemmy-js-client";
+import { PostView } from "lemmy-js-client";
 import { HStack, useTheme, View } from "native-base";
 import { Button, RefreshControl, StyleSheet } from "react-native";
-import { useActionSheet } from "@expo/react-native-action-sheet";
 import { FlashList, ListRenderItemInfo } from "@shopify/flash-list";
 import { useNavigation, useScrollToTop } from "@react-navigation/native";
-import { trigger } from "react-native-haptic-feedback";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import {
-  IconBolt,
-  IconBookmark,
-  IconCalendar,
-  IconClockHour4,
-  IconDots,
-  IconFlame,
-  IconMapPin,
-  IconMessage,
-  IconWorld,
-} from "tabler-icons-react-native";
 import FeedItem from "./FeedItem";
 import LoadingView from "../Loading/LoadingView";
 import FeedHeaderDropdownDrawer from "./FeedHeaderDropdownDrawer";
 import { useAppDispatch, useAppSelector } from "../../../store";
 import { selectFeed, setDropdownVisible } from "../../../slices/feed/feedSlice";
 import { UseFeed } from "../../hooks/feeds/useFeed";
-import LoadingFooter from "../Loading/LoadingFooter";
-import LoadingErrorFooter from "../Loading/LoadingErrorFooter";
-import { lemmyAuthToken, lemmyInstance } from "../../../lemmy/LemmyInstance";
 import LoadingErrorView from "../Loading/LoadingErrorView";
 import CompactFeedItem from "./CompactFeedItem/CompactFeedItem";
 import { selectSettings } from "../../../slices/settings/settingsSlice";
 import { ExtensionType, getLinkInfo } from "../../../helpers/LinkHelper";
-import HeaderIconButton from "../buttons/HeaderIconButton";
-import { IconCalendarWeek } from "../customIcons";
-import { showToast } from "../../../slices/toast/toastSlice";
 import NoResultView from "../common/NoResultView";
-import { Community, FeedOverflowButton } from "./FeedOverflowButton";
+import { Footer } from "./Footer";
 import { FeedSortButton } from "./FeedSortButton";
+import { Community, FeedOverflowButton } from "./FeedOverflowButton";
 
 interface FeedViewProps {
   feed: UseFeed;
   community?: boolean;
   header?: () => JSX.Element | null;
 }
-
-const SortIconType = {
-  TopDay: <IconCalendar />,
-  TopWeek: <IconCalendarWeek />,
-  Hot: <IconFlame />,
-  Active: <IconBolt />,
-  New: <IconClockHour4 />,
-  MostComments: <IconMessage />,
-};
-
-const ContextualMenuIconType = {
-  Ellipses: <IconDots />,
-  All: <IconWorld />,
-  Local: <IconMapPin />,
-  Subscribed: <IconBookmark />,
-};
 
 function FeedView({ feed, community = false, header }: FeedViewProps) {
   // State Props
@@ -87,12 +53,6 @@ function FeedView({ feed, community = false, header }: FeedViewProps) {
     },
     []
   );
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => headerRight(),
-    });
-  }, [feed.sort, feed.listingType]);
 
   const firstPost = (feed.posts?.length ?? 0) > 0 ? feed.posts[0] : undefined;
 
@@ -183,32 +143,7 @@ function FeedView({ feed, community = false, header }: FeedViewProps) {
     },
     []
   );
-  const headerRight = () => {
-    if (dropdownVisible) {
-      return (
-        <Button title="Cancel" onPress={() => dispatch(setDropdownVisible())} />
-      );
-    }
 
-    return (
-      <HStack space={1}>
-        <HeaderIconButton
-          icon={SortIconType[feed.sort]}
-          onPress={onSortPress}
-        />
-        <HeaderIconButton
-          icon={
-            community
-              ? ContextualMenuIconType.Ellipses
-              : ContextualMenuIconType[feed.listingType]
-          }
-          onPress={onContextualMenuButtonPress}
-        />
-      </HStack>
-    );
-  };
-
-  const keyExtractor = (item) => item.post.id.toString();
   const refreshControl = (
     <RefreshControl
       refreshing={feed.postsLoading}
@@ -216,27 +151,6 @@ function FeedView({ feed, community = false, header }: FeedViewProps) {
       tintColor={theme.colors.app.textSecondary}
     />
   );
-
-  const footer = () => {
-    if ((feed.postsLoading && feed.posts.length > 0) || endReached) {
-      return <LoadingFooter message="Loading more posts..." />;
-    }
-    if (feed.postsError) {
-      if (!feed.posts || feed.posts.length < 1) {
-        return <LoadingErrorView onRetryPress={feed.doLoad} />;
-      }
-
-      return (
-        <LoadingErrorFooter
-          onRetryPress={() => feed.doLoad(true)}
-          message="Failed to load posts :("
-        />
-      );
-    }
-    return null;
-  };
-
-  const HeaderComponent = header;
 
   return (
     <View style={styles.container} backgroundColor={theme.colors.app.bg}>
@@ -248,22 +162,31 @@ function FeedView({ feed, community = false, header }: FeedViewProps) {
         )) ||
         (feed.community && feed.community.counts.posts < 1 && (
           <>
-            <HeaderComponent />
+            {header}
             <NoResultView type="posts" />
           </>
         )) || (
           <FlashList
             ListHeaderComponent={header}
             data={feed.posts}
-            extraData={feed.refreshList}
             renderItem={renderItem}
             keyExtractor={keyExtractor}
+            extraData={feed.refreshList}
             refreshControl={refreshControl}
             onEndReachedThreshold={0.5}
-            estimatedItemSize={compactView ? 100 : 500}
-            ListFooterComponent={footer}
-            ListEmptyComponent={<NoResultView type="posts" />}
             onEndReached={onEndReached}
+            estimatedItemSize={compactView ? 100 : 500}
+            ListFooterComponent={
+              <Footer
+                loading={
+                  (feed.postsLoading && feed.posts.length > 0) || endReached
+                }
+                error={feed.postsError}
+                empty={(feed.posts ?? []).length === 0}
+                onRetry={feed.doLoad}
+              />
+            }
+            ListEmptyComponent={<NoResultView type="posts" />}
             ref={flashList}
             getItemType={getItemType}
           />
@@ -271,6 +194,8 @@ function FeedView({ feed, community = false, header }: FeedViewProps) {
     </View>
   );
 }
+
+const keyExtractor = (item: PostView) => item.post.id.toString();
 
 const styles = StyleSheet.create({
   container: {
