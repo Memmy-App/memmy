1b0bce62a973c0ae7f89c0f2aeaa4a56cad15b0a
Gavin Kasdorf <me@gkasdorf.com>
2023-06-30 18:59:05 -0800

    change icons

--- components/screens/post/NewPostBodyScreen.tsx
+++ components/screens/post/NewPostBodyScreen.tsx
@@ -39,37 +39,39 @@ function NewPostBodyScreen({ route, navigation }: IProps) {
   };
 
   return (
-    <KeyboardAwareScrollView style={{ backgroundColor: theme.colors.app.bg }}>
-      <TextInput
-        multiline
-        placeholder="Type away!"
-        placeholderTextColor={theme.colors.app.textSecondary}
-        autoCapitalize="sentences"
-        style={[
-          styles.input,
-          {
-            backgroundColor: theme.colors.app.bg,
-            color: theme.colors.app.textPrimary,
-          },
-        ]}
-        numberOfLines={20}
-        value={body}
-        onSelectionChange={(e) => {
-          setSelection(e.nativeEvent.selection);
-        }}
-        onChangeText={setBody}
-        keyboardAppearance={theme.config.initialColorMode}
-        inputAccessoryViewID="accessory"
-        ref={textInput}
-        autoFocus
-      />
+    <>
+      <KeyboardAwareScrollView style={{ backgroundColor: theme.colors.app.bg }}>
+        <TextInput
+          multiline
+          placeholder="Type away!"
+          placeholderTextColor={theme.colors.app.textSecondary}
+          autoCapitalize="sentences"
+          style={[
+            styles.input,
+            {
+              backgroundColor: theme.colors.app.bg,
+              color: theme.colors.app.textPrimary,
+            },
+          ]}
+          numberOfLines={20}
+          value={body}
+          onSelectionChange={(e) => {
+            setSelection(e.nativeEvent.selection);
+          }}
+          onChangeText={setBody}
+          keyboardAppearance={theme.config.initialColorMode}
+          inputAccessoryViewID="accessory"
+          ref={textInput}
+          autoFocus
+        />
+      </KeyboardAwareScrollView>
       <KeyboardAccessory
         setText={setBody}
         text={body}
         selection={selection}
         inputRef={textInput}
       />
-    </KeyboardAwareScrollView>
+    </>
   );
 }
 
