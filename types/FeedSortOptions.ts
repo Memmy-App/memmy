d740e90f10d974edb0665670ef5555c930f10361
Gavin Kasdorf <me@gkasdorf.com>
2023-07-05 03:15:55 -0700

    add top hour, six hours, twelve hours

--- types/FeedSortOptions.ts
+++ types/FeedSortOptions.ts
@@ -5,6 +5,9 @@ export type SortOption = [key: SortType, display: string];
 export const sortOptions = [
   ["TopDay", "Top Day"],
   ["TopWeek", "Top Week"],
+  ["TopHour", "Top Hour"],
+  ["TopSixHour", "Top Six Hours"],
+  ["TopTwelveHour", "Top Twelve Hours"],
   ["Hot", "Hot"],
   ["Active", "Active"],
   ["New", "New"],
