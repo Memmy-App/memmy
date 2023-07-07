8d22d6bbbb8b9c0b168982dcd6a8c56550f7817e
David Lomas <david.lomas@knowinnovation.com>
2023-07-07 15:35:56 +0100

    Add option for reader mode in web view

--- src/slices/settings/settingsSlice.ts
+++ src/slices/settings/settingsSlice.ts
@@ -33,6 +33,7 @@ export interface SettingsState {
   markReadOnPostVote: boolean;
   ignoreScreenHeightInFeed: boolean;
   displayTotalScore: boolean;
+  useReaderMode: boolean;
   accentColor?: string;
   hideReadPostsOnFeed: boolean;
 }
@@ -65,6 +66,7 @@ const initialState: SettingsState = {
   markReadOnPostVote: true,
   ignoreScreenHeightInFeed: false,
   displayTotalScore: true,
+  useReaderMode: false,
   hideReadPostsOnFeed: false,
 };
 
