cc3e9093a8414deae944e9baaa27f48594e8ec13
Gavin Kasdorf <me@gkasdorf.com>
2023-07-07 17:04:36 -0700

    fix ci script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-# Install CocoaPods and yarn using Homebrew.
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
+brew install node@18
+brew link node@18
+
 brew install yarn
 
-# Install dependencies
-yarn
+yarn install
 pod install
+
+sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
