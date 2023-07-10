be75e6b558ffed010f631697538c02e17b2ddf61
Sean Griffin <sgriffin6697@gmail.com>
2023-07-09 20:00:58 -0700

    Fix postview logic

--- src/components/screens/Post/components/PostContentView.tsx
+++ src/components/screens/Post/components/PostContentView.tsx
@@ -1,13 +1,11 @@
 import { PostView } from "lemmy-js-client";
-import { Box, VStack } from "native-base";
+import { Box } from "native-base";
 import React from "react";
 import { ExtensionType, getLinkInfo } from "../../../../helpers/LinkHelper";
 import LinkButton from "../../../common/Buttons/LinkButton";
 import ImageViewer from "../../../common/ImageViewer/ImageViewer";
 import RenderMarkdown from "../../../common/Markdown/RenderMarkdown";
 import PostTitle from "./PostTitle";
-// eslint-disable-next-line import/no-extraneous-dependencies
-// eslint-disable-next-line import/no-extraneous-dependencies
 
 interface IProps {
   post: PostView;
@@ -27,62 +25,39 @@ function PostContentView({ post }: IProps) {
       linkInfo.extType === ExtensionType.GENERIC
     ) {
       return (
-        <VStack>
-          <Box mx={4}>
-            <LinkButton
-              link={linkInfo.link}
-              thumbnail={post.post.thumbnail_url}
-            />
-          </Box>
-        </VStack>
+        <Box mx={4}>
+          <LinkButton
+            link={linkInfo.link}
+            thumbnail={post.post.thumbnail_url}
+          />
+        </Box>
       );
     }
 
     return null;
   };
 
-  const renderContent = () => {
-    if (isImage) {
-      return (
-        <VStack>
-          <ImageViewer
-            source={post.post.url}
-            nsfw={post.post.nsfw || post.community.nsfw}
-            id={post.post.id}
-          />
-          <PostTitle title={title} mt={2} mb={0} />
-          {body && (
-            <Box mx={4}>
-              <RenderMarkdown text={body} addImages truncate={false} />
-            </Box>
-          )}
-        </VStack>
-      );
-    }
+  return (
+    <Box mb={1}>
+      {isImage && (
+        <ImageViewer
+          source={post.post.url}
+          nsfw={post.post.nsfw || post.community.nsfw}
+          id={post.post.id}
+        />
+      )}
 
-    if (body) {
-      return (
-        <VStack>
-          <>
-            <PostTitle title={title} mt={2} mb={0} />
-            <Box mx={4}>
-              <RenderMarkdown text={body} addImages truncate={false} />
-            </Box>
-            {linkInfoBlock()}
-          </>
-        </VStack>
-      );
-    }
+      <PostTitle title={title} mt={2} mb={isImage ? 1 : 0} />
 
-    return (
-      <VStack>
-        <PostTitle title={title} mt={2} mb={2} />
-        {linkInfoBlock()}
-      </VStack>
-    );
-  };
+      {!!body && (
+        <Box mx={4}>
+          <RenderMarkdown text={body} addImages truncate={false} />
+        </Box>
+      )}
 
-  return <Box mb={1}>{renderContent()}</Box>;
+      {linkInfoBlock()}
+    </Box>
+  );
 }
 
 const areEqual = (prev: IProps, next: IProps) =>
