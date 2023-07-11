fffb5048cd4f49f9fbec7a40a2afa5c1765cf0d4
Gavin Kasdorf <me@gkasdorf.com>
2023-07-11 01:18:37 -0700

    logic for handling not_logged_in, which indicates an expired token.

--- src/helpers/LemmyErrorHelper.ts
+++ src/helpers/LemmyErrorHelper.ts
@@ -6,6 +6,10 @@ import {
 } from "../types/lemmy/LemmyErrors";
 import store from "../../store";
 import { showToast } from "../slices/toast/toastSlice";
+import {
+  deleteAccount,
+  setCurrentAccount,
+} from "../slices/accounts/accountsActions";
 
 export const handleLemmyError = (code: LemmyErrorType | string) => {
   // Find the error if it exists
@@ -17,6 +21,40 @@ export const handleLemmyError = (code: LemmyErrorType | string) => {
       "Unknown Error",
       `An unknown error has occurred. Code: ${code}`
     );
+    return;
+  }
+
+  // If the error is not_logged_in, then we know the token has expired. We remove the account from redux and either
+  // switch the user to another account or (if no other account exists) the conditional inside of Stack will direct
+  // them to log in
+  if (error.code === "not_logged_in") {
+    Alert.alert(
+      "Error",
+      "Your session has expired. Please sign back in to this account."
+    );
+
+    // Get the current account
+    const { currentAccount, accounts } = store.getState().accounts;
+
+    // We should change the user to a different account *before* removing the account, if possible
+    const accountIndex = accounts.findIndex(
+      (a) =>
+        a.username === currentAccount.username &&
+        a.instance === currentAccount.instance
+    );
+
+    // If there are additional accounts we will switch to that one.
+    if (accounts.length > 1) {
+      if (accountIndex === 0) {
+        store.dispatch(setCurrentAccount(accounts[1]));
+      } else {
+        store.dispatch(setCurrentAccount(accounts[0]));
+      }
+    }
+
+    // Delete the account
+    store.dispatch(deleteAccount(currentAccount));
+
     return;
   }
 
