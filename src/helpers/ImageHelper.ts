d43ebf1396fa343f0565c4ea40711d9ca74db434
Gavin Kasdorf <me@gkasdorf.com>
2023-07-14 22:53:07 -0700

    delete image after share

--- src/helpers/ImageHelper.ts
+++ src/helpers/ImageHelper.ts
@@ -26,6 +26,18 @@ export const downloadImage = async (src: string): Promise<string | boolean> => {
   }
 };
 
+export const deleteImage = async (uri: string): Promise<boolean> => {
+  try {
+    await FileSystem.deleteAsync(uri);
+    console.log("Image deleted.");
+    return true;
+  } catch (e) {
+    writeToLog("Error deleting file.");
+    writeToLog(e.toString());
+    return false;
+  }
+};
+
 const downloadAndSaveImage = async (src: string): Promise<boolean> => {
   const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
 
