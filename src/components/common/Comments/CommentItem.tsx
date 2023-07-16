89f31dd0989c44feb0b828baa0cc74ffa1fe4b4f
Gavin Kasdorf <127079571+gkasdorf@users.noreply.github.com>
2023-07-16 16:49:00 -0700

    Some Performance Fixes (#714)

--- src/components/common/Comments/CommentItem.tsx
+++ src/components/common/Comments/CommentItem.tsx
@@ -1,18 +1,5 @@
-import {
-  Divider,
-  HStack,
-  Pressable,
-  Text,
-  useTheme,
-  View,
-  VStack,
-} from "native-base";
+import { Divider, HStack, useTheme, View } from "native-base";
 import React from "react";
-import {
-  IconChevronDown,
-  IconDots,
-  IconMessagePlus,
-} from "tabler-icons-react-native";
 import useComment from "../../../hooks/post/useComment";
 import ILemmyComment from "../../../types/lemmy/ILemmyComment";
 import { ILemmyVote } from "../../../types/lemmy/ILemmyVote";
@@ -28,8 +15,11 @@ import { CommentContextMenu } from "./CommentContextMenu";
 import AvatarUsername from "../AvatarUsername";
 import SmallVoteIcons from "../Vote/SmallVoteIcons";
 import IconButtonWithText from "../IconButtonWithText";
-import { timeFromNowShort } from "../../../helpers/TimeHelper";
 import VoteButton from "../Vote/VoteButton";
+import CommentWrapper from "./CommentWrapper";
+import CommentHeaderWrapper from "./CommentHeader/CommentHeaderWrapper";
+import SFIcon from "../icons/SFIcon";
+import CommentHeaderRight from "./CommentHeader/CommentHeaderRight";
 
 interface IProps {
   comment: ILemmyComment;
@@ -83,124 +73,73 @@ function CommentItem({
           }}
           options={commentHook.longPressOptions}
         >
-          <Pressable onPress={commentHook.onCommentPress}>
-            <VStack
-              flex={1}
-              pr={2}
-              space={2}
-              backgroundColor={theme.colors.app.fg}
-              style={{
-                paddingLeft: depth * 8,
-              }}
-              py={1}
-            >
-              <VStack
-                borderLeftWidth={depth > 2 ? 2 : 0}
-                borderLeftColor={
-                  theme.colors.app.comments[depth - 2] ??
-                  theme.colors.app.comments[5]
-                }
-                borderLeftRadius={1}
-                pl={depth > 2 ? 2 : 0}
-                mt={0}
-              >
-                <HStack
-                  space={2}
-                  justifyContent="space-between"
-                  alignItems="center"
-                  mb={-3}
-                  pb={2}
-                >
-                  <AvatarUsername
-                    creator={comment.comment.creator}
-                    opId={comment.comment.post.creator_id}
-                  >
-                    <SmallVoteIcons
-                      upvotes={comment.comment.counts.upvotes}
-                      downvotes={comment.comment.counts.downvotes}
-                      myVote={comment.comment.my_vote as ILemmyVote}
+          <CommentWrapper
+            depth={depth}
+            onCommentPress={commentHook.onCommentPress}
+          >
+            <CommentHeaderWrapper>
+              <HStack space={1}>
+                <AvatarUsername
+                  creator={comment.comment.creator}
+                  opId={comment.comment.post.creator_id}
+                />
+                <SmallVoteIcons
+                  upvotes={comment.comment.counts.upvotes}
+                  downvotes={comment.comment.counts.downvotes}
+                  myVote={comment.comment.my_vote as ILemmyVote}
+                />
+              </HStack>
+              <CommentHeaderRight
+                published={comment.comment.comment.published}
+                onPress={commentHook.onCommentLongPress}
+                contextOptions={commentHook.longPressOptions}
+                collapsed={comment.collapsed}
+              />
+            </CommentHeaderWrapper>
+            {comment.collapsed ? (
+              <CommentCollapsed />
+            ) : (
+              <>
+                <CommentBody
+                  deleted={comment.comment.comment.deleted}
+                  removed={comment.comment.comment.removed}
+                  content={comment.comment.comment.content}
+                  instance={getBaseUrl(comment.comment.comment.ap_id)}
+                />
+                {settings.showCommentActions && (
+                  <HStack justifyContent="flex-end" space={2} mb={1}>
+                    <IconButtonWithText
+                      onPressHandler={commentHook.onReply}
+                      icon={<SFIcon icon="plus.message" />}
                     />
-                  </AvatarUsername>
-                  {!comment.collapsed ? (
-                    <HStack alignItems="center" space={2}>
-                      <CommentContextMenu
-                        isShortPress
-                        options={commentHook.longPressOptions}
-                        onPress={({ nativeEvent }) => {
-                          commentHook.onCommentLongPress(nativeEvent.actionKey);
-                        }}
-                      >
-                        <IconButtonWithText
-                          icon={
-                            <IconDots
-                              size={24}
-                              color={theme.colors.app.textSecondary}
-                            />
-                          }
-                        />
-                      </CommentContextMenu>
-                      <Text color={theme.colors.app.textSecondary}>
-                        {timeFromNowShort(comment.comment.comment.published)}
-                      </Text>
-                    </HStack>
-                  ) : (
-                    <IconChevronDown
-                      size={24}
-                      color={theme.colors.app.textSecondary}
+                    <VoteButton
+                      onPressHandler={async () =>
+                        comment.comment.my_vote === 1
+                          ? commentHook.onVote(0)
+                          : commentHook.onVote(1)
+                      }
+                      type="upvote"
+                      isVoted={comment.comment.my_vote === 1}
+                      isAccented
+                      iconSize={22}
                     />
-                  )}
-                </HStack>
-                {comment.collapsed ? (
-                  <CommentCollapsed />
-                ) : (
-                  <>
-                    <CommentBody
-                      deleted={comment.comment.comment.deleted}
-                      removed={comment.comment.comment.removed}
-                      content={comment.comment.comment.content}
-                      instance={getBaseUrl(comment.comment.comment.ap_id)}
+                    <VoteButton
+                      onPressHandler={async () =>
+                        comment.comment.my_vote === -1
+                          ? commentHook.onVote(0)
+                          : commentHook.onVote(-1)
+                      }
+                      type="downvote"
+                      isVoted={comment.comment.my_vote === -1}
+                      isAccented
+                      iconSize={22}
+                      textSize="md"
                     />
-                    {settings.showCommentActions && (
-                      <HStack justifyContent="flex-end" space={2} mb={1}>
-                        <IconButtonWithText
-                          onPressHandler={commentHook.onReply}
-                          icon={
-                            <IconMessagePlus
-                              color={theme.colors.app.accent}
-                              size={22}
-                            />
-                          }
-                        />
-                        <VoteButton
-                          onPressHandler={async () =>
-                            comment.comment.my_vote === 1
-                              ? commentHook.onVote(0)
-                              : commentHook.onVote(1)
-                          }
-                          type="upvote"
-                          isVoted={comment.comment.my_vote === 1}
-                          isAccented
-                          iconSize={22}
-                        />
-                        <VoteButton
-                          onPressHandler={async () =>
-                            comment.comment.my_vote === -1
-                              ? commentHook.onVote(0)
-                              : commentHook.onVote(-1)
-                          }
-                          type="downvote"
-                          isVoted={comment.comment.my_vote === -1}
-                          isAccented
-                          iconSize={22}
-                          textSize="md"
-                        />
-                      </HStack>
-                    )}
-                  </>
+                  </HStack>
                 )}
-              </VStack>
-            </VStack>
-          </Pressable>
+              </>
+            )}
+          </CommentWrapper>
         </CommentContextMenu>
       </SwipeableRow>
       <View
@@ -215,13 +154,4 @@ function CommentItem({
   );
 }
 
-const areEqual = (prev: IProps, next: IProps) =>
-  prev.comment.comment.comment.id === next.comment.comment.comment.id &&
-  prev.comment.comment.my_vote === next.comment.comment.my_vote &&
-  prev.comment.collapsed === next.comment.collapsed &&
-  prev.isUnreadReply === next.isUnreadReply &&
-  prev.comment.comment.comment.deleted ===
-    next.comment.comment.comment.deleted &&
-  prev.comment.comment.comment.content === next.comment.comment.comment.content;
-
-export default React.memo(CommentItem, areEqual);
+export default React.memo(CommentItem);
