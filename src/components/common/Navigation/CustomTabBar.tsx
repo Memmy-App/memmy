d63b8888b7b23b5fef7104aabf71a3c1f9bd7bf6
Gavin Kasdorf <127079571+gkasdorf@users.noreply.github.com>
2023-07-16 18:21:08 -0700

    feat: add gestures to tab bar (#715)

--- src/components/common/Navigation/CustomTabBar.tsx
+++ src/components/common/Navigation/CustomTabBar.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import { AccountsContextMenu } from "../ContextMenu/AccountsContextMenu";
 import { onGenericHapticFeedback } from "../../../helpers/HapticFeedbackHelpers";
+import TabBarGestureHandler from "./TabBarGestureHandler";
 
 function IconWithText({
   icon,
@@ -33,91 +34,95 @@ export function CustomTabBar({
   const { colors } = useTheme();
 
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        height: 75,
-        backgroundColor: colors.app.navBarBg,
-        paddingTop: 6,
-        paddingHorizontal: 5,
-        borderTopWidth: 1,
-        borderTopColor: colors.app.border,
-      }}
-    >
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
+    <TabBarGestureHandler>
+      <View
+        style={{
+          flexDirection: "row",
+          height: 75,
+          backgroundColor: colors.app.navBarBg,
+          paddingTop: 6,
+          paddingHorizontal: 5,
+          borderTopWidth: 1,
+          borderTopColor: colors.app.border,
+        }}
+      >
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key];
 
-        const label = options.tabBarLabel ? options.tabBarLabel : "";
+          const label = options.tabBarLabel ? options.tabBarLabel : "";
 
-        const isFocused = state.index === index;
+          const isFocused = state.index === index;
 
-        const color = isFocused ? colors.app.accent : colors.app.textSecondary;
+          const color = isFocused
+            ? colors.app.accent
+            : colors.app.textSecondary;
 
-        const icon = options.tabBarIcon({
-          color,
-          focused: isFocused,
-          size: 30,
-        });
-
-        const onPress = () => {
-          onGenericHapticFeedback();
-          const event = navigation.emit({
-            type: "tabPress",
-            target: route.key,
-            canPreventDefault: true,
+          const icon = options.tabBarIcon({
+            color,
+            focused: isFocused,
+            size: 30,
           });
 
-          if (!isFocused && !event.defaultPrevented) {
-            // The `merge: true` option makes sure that the params inside the tab screen are preserved
-            // @ts-ignore this is valid, types are wrong from React Navigation
-            navigation.navigate({ name: route.name, merge: true });
+          const onPress = () => {
+            onGenericHapticFeedback();
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!isFocused && !event.defaultPrevented) {
+              // The `merge: true` option makes sure that the params inside the tab screen are preserved
+              // @ts-ignore this is valid, types are wrong from React Navigation
+              navigation.navigate({ name: route.name, merge: true });
+            }
+          };
+
+          const onLongPress = () => {
+            onGenericHapticFeedback();
+
+            navigation.emit({
+              type: "tabLongPress",
+              target: route.key,
+            });
+          };
+
+          if (route.name === "ProfileStack") {
+            return (
+              <AccountsContextMenu navigation={navigation}>
+                <TouchableOpacity
+                  accessibilityRole="button"
+                  accessibilityState={isFocused ? { selected: true } : {}}
+                  accessibilityLabel={options.tabBarAccessibilityLabel}
+                  testID={options.tabBarTestID}
+                  onPress={onPress}
+                  onLongPress={onLongPress}
+                >
+                  <IconWithText
+                    icon={icon}
+                    label={label as string}
+                    color={color}
+                  />
+                </TouchableOpacity>
+              </AccountsContextMenu>
+            );
           }
-        };
 
-        const onLongPress = () => {
-          onGenericHapticFeedback();
-
-          navigation.emit({
-            type: "tabLongPress",
-            target: route.key,
-          });
-        };
-
-        if (route.name === "ProfileStack") {
           return (
-            <AccountsContextMenu navigation={navigation}>
-              <TouchableOpacity
-                accessibilityRole="button"
-                accessibilityState={isFocused ? { selected: true } : {}}
-                accessibilityLabel={options.tabBarAccessibilityLabel}
-                testID={options.tabBarTestID}
-                onPress={onPress}
-                onLongPress={onLongPress}
-              >
-                <IconWithText
-                  icon={icon}
-                  label={label as string}
-                  color={color}
-                />
-              </TouchableOpacity>
-            </AccountsContextMenu>
+            <TouchableOpacity
+              accessibilityRole="button"
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={{ flex: 1 }}
+            >
+              <IconWithText icon={icon} label={label as string} color={color} />
+            </TouchableOpacity>
           );
-        }
-
-        return (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={{ flex: 1 }}
-          >
-            <IconWithText icon={icon} label={label as string} color={color} />
-          </TouchableOpacity>
-        );
-      })}
-    </View>
+        })}
+      </View>
+    </TabBarGestureHandler>
   );
 }
