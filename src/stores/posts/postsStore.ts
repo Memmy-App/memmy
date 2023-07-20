5285a3651f1c27452a0230fc89969f74d9ca6028
Gavin Kasdorf <me@gkasdorf.com>
2023-07-19 21:22:32 -0700

    reorg selectors

--- src/stores/posts/postsStore.ts
+++ src/stores/posts/postsStore.ts
@@ -25,39 +25,38 @@ export interface PostState {
   rerenderComments: boolean;
 }
 
+// Create our store
 export const usePostsStore = create(
   immer<PostsStore>(() => ({
     posts: new Map<string, PostState>(),
   }))
 );
 
-export const useCurrentPostState = (postKey: string) =>
-  usePostsStore((state) => state.posts.get(postKey));
+// Post state
 export const useCurrentPost = (postKey: string) =>
   usePostsStore((state) => state.posts.get(postKey).post);
-
-export const usePostComments = (postKey: string) =>
-  usePostsStore((state) => state.posts.get(postKey).commentsState.comments);
-
 export const usePostTitle = (postKey: string) =>
   usePostsStore((state) => state.posts.get(postKey).post.post.name);
 export const usePostCollapsed = (postKey: string) =>
   usePostsStore((state) => state.posts.get(postKey).collapsed);
+export const usePostRerenderComments = (postKey: string) =>
+  usePostsStore((state) => state.posts.get(postKey).rerenderComments);
 
+// Comment state
+export const useCurrentPostState = (postKey: string) =>
+  usePostsStore((state) => state.posts.get(postKey));
+export const usePostComments = (postKey: string) =>
+  usePostsStore((state) => state.posts.get(postKey).commentsState.comments);
+export const usePostCommentsStatus = (postKey: string) =>
+  usePostsStore((state) => ({
+    commentsLoading: state.posts.get(postKey).commentsState.commentsLoading,
+    commentsError: state.posts.get(postKey).commentsState.commentsError,
+  }));
+export const usePostCommentsSort = (postKey: string) =>
+  usePostsStore((state) => state.posts.get(postKey).commentsState.commentsSort);
 export const usePostComment = (postKey: string, commentId: number) =>
   usePostsStore((state) =>
     state.posts
       .get(postKey)
       .commentsState.comments.find((c) => c.comment.comment.id === commentId)
   );
-
-export const usePostRerenderComments = (postKey: string) =>
-  usePostsStore((state) => state.posts.get(postKey).rerenderComments);
-
-export const usePostCommentsSort = (postKey: string) =>
-  usePostsStore((state) => state.posts.get(postKey).commentsState.commentsSort);
-export const usePostCommentsStatus = (postKey: string) =>
-  usePostsStore((state) => ({
-    commentsLoading: state.posts.get(postKey).commentsState.commentsLoading,
-    commentsError: state.posts.get(postKey).commentsState.commentsError,
-  }));
