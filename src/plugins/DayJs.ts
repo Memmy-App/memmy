38acc1d7032846225ace9f5eda5293e3284b4ce7
Felix <felix.staud@gmail.com>
2023-08-03 08:31:30 +0200

    language fallback
    - add language fallback method
    - rename translation files to match <locale>-<Country-Code> pattern

--- src/plugins/DayJs.ts
+++ src/plugins/DayJs.ts
@@ -6,11 +6,11 @@ import advancedFormat from "dayjs/plugin/advancedFormat";
 import relativeTime from "dayjs/plugin/relativeTime";
 import updateLocale from "dayjs/plugin/updateLocale";
 import utc from "dayjs/plugin/utc";
-import german from "./i18n/locales/de.json";
-import english from "./i18n/locales/en.json";
-import czech from "./i18n/locales/cz.json";
-import romanian from "./i18n/locales/ro.json";
-import portuguese from "./i18n/locales/pt_br.json";
+import german from "./i18n/locales/de_DE.json";
+import english from "./i18n/locales/en_US.json";
+import czech from "./i18n/locales/cs_CZ.json";
+import romanian from "./i18n/locales/ro_RO.json";
+import portuguese from "./i18n/locales/pt_BR.json";
 
 // extend dayjs with plugins
 dayjs.extend(advancedFormat);
--- src/plugins/i18n/i18n.ts
+++ src/plugins/i18n/i18n.ts
@@ -1,34 +1,66 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import english from "./locales/en.json";
-import german from "./locales/de.json";
-import brazilian_portuguese from "./locales/pt_br.json";
-import czech from "./locales/cz.json";
-import romanian from "./locales/ro.json";
+import { logMessage } from "@react-native-community/cli-doctor/build/tools/healthchecks/common";
 import languageDetector from "./languageDetector";
+import en_US from "./locales/en_US.json";
+import de_DE from "./locales/de_DE.json";
+import pt_BR from "./locales/pt_BR.json";
+import cs_CZ from "./locales/cs_CZ.json";
+import ro_RO from "./locales/ro_RO.json";
+
+type SupportedLocale = "en" | "de" | "pt" | "ro" | "cs";
 
 i18n
   .use(languageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      en: {
-        translation: english,
+      "en-US": {
+        translation: en_US,
       },
-      de: {
-        translation: german,
+      "de-DE": {
+        translation: de_DE,
       },
-      pt: {
-        translation: brazilian_portuguese,
+      "pt-BR": {
+        translation: pt_BR,
       },
-      ro: {
-        translation: romanian,
+      "ro-RO": {
+        translation: ro_RO,
       },
-      cz: {
-        translation: czech,
+      "cs-CZ": {
+        translation: cs_CZ,
       },
     },
-    fallbackLng: "en",
+    fallbackLng: (code) => {
+      const fallbacks = ["en-US"];
+
+      if (!code) return fallbacks;
+
+      const [locale] = code.split("-") as [SupportedLocale, string];
+
+      switch (locale) {
+        case "en":
+          // already covered
+          break;
+        case "de":
+          fallbacks.push("de-DE");
+          break;
+        case "pt":
+          fallbacks.push("pt-BR");
+          break;
+        case "ro":
+          fallbacks.push("ro-RO");
+          break;
+        case "cs":
+          fallbacks.push("cs-CZ");
+          break;
+        default:
+          logMessage(`Language "${code}" not supported yet.`);
+          break;
+      }
+
+      return fallbacks.reverse();
+    },
     interpolation: {
       escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
     },
