abfd8f36ba6190257be61fdce4bb5c8442909bcd
Gavin <me@gkasdorf.com>
2023-10-19 22:00:01 -0700

    replace all ellipsis

--- src/components/Comment/components/CommentEllipsisButton.tsx
+++ src/components/Comment/components/CommentEllipsisButton.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { CircleEllipsis } from '@tamagui/lucide-icons';
-import AnimatedIconButton from '@components/Common/Button/AnimatedIconButton';
 import CommentContextMenu from '@components/Common/ContextMenu/components/CommentContextMenu';
+import Ellipsis from '@components/Common/Icons/Ellipsis';
+import { useTheme } from 'tamagui';
+import { Pressable } from 'react-native';
 
 interface IProps {
   itemId: number;
@@ -10,9 +11,13 @@ interface IProps {
 export default function CommentEllipsisButton({
   itemId,
 }: IProps): React.JSX.Element {
+  const theme = useTheme();
+
   return (
     <CommentContextMenu itemId={itemId}>
-      <AnimatedIconButton icon={CircleEllipsis} iconSize={16} />
+      <Pressable>
+        <Ellipsis size={16} color={theme.accent.val} />
+      </Pressable>
     </CommentContextMenu>
   );
 }
