393f4516d1ea9d027969335f7448decc7f62b58e
Gavin <me@gkasdorf.com>
2023-10-26 18:24:42 -0700

    [chore] Update deploy

--- bin/deploy-ios-github.sh
+++ bin/deploy-ios-github.sh
@@ -1,8 +1,15 @@
-# Make sure that all our packages are installed
-yarn install
+# Add fastlane plugins
+cd ios
+gem install fastlane
+fastlane add_plugin changelog
+cd ..
 
 # Make sure that pods are installed
 cd ios && pod install && cd ..
+
+# Configure the git environment
+git config user.name "GitHub Actions Bot"
+git config user.email "<>"
 
 # Generate updated license
 yarn generate-license
