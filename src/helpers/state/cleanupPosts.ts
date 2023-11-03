550e945d093a53e6bb1aef7801bf5469a96a2e0e
Gavin <me@gkasdorf.com>
2023-11-03 01:00:24 -0700

    [fix] fix removing of posts

--- src/helpers/state/cleanupPosts.ts
+++ src/helpers/state/cleanupPosts.ts
@@ -1,18 +1,18 @@
 import { removePost, useDataStore } from '@src/state';
 
 export const cleanupPosts = (screenId: string): void => {
+  const postIds = useDataStore.getState().feeds.get(screenId)?.postIds ?? [];
+
+  for (let i = 0; i < postIds.length; i++) {
+    const postId = postIds[i];
+
+    removePost({
+      postId,
+      screenIdOrOverride: screenId,
+    });
+  }
+
   useDataStore.setState((state) => {
-    const postIds = state.feeds.get(screenId)?.postIds ?? [];
-
-    for (let i = 0; i < postIds.length; i++) {
-      const postId = postIds[i];
-
-      removePost({
-        postId,
-        screenIdOrOverride: screenId,
-      });
-    }
-
     state.feeds.delete(screenId);
   });
 };
