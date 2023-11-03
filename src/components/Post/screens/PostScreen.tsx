9c5c30315fa76449428b01a3a708d3ced4c00f60
Gavin <127079571+gkasdorf@users.noreply.github.com>
2023-11-02 19:45:36 -0700

    [feat] refresh post on open (#1057)

--- src/components/Post/screens/PostScreen.tsx
+++ src/components/Post/screens/PostScreen.tsx
@@ -1,26 +1,13 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import {
-  setNewCommentId,
-  useDefaultCommentSort,
-  useNewCommentId,
-  usePostCommentsInfo,
-  usePostCommunityId,
-  usePostCounts,
-} from '@src/state';
+import React from 'react';
 import Post from '@components/Post/components/Post';
-import instance from '@src/Instance';
 import { ICommentInfo } from '@src/types';
 import CommentChain from '@components/Comment/components/CommentChain';
-import { useLoadData } from '@hooks/useLoadData';
 import FeedLoadingIndicator from '@components/Feed/components/Feed/FeedLoadingIndicator';
 import { FlashList, ListRenderItemInfo } from '@shopify/flash-list';
-import { CommentSortType } from 'lemmy-js-client';
-import { stripEss } from '@helpers/text';
-import CommentSortTypeContextMenuButton from '@components/Common/ContextMenu/components/buttons/CommentSortTypeContextMenuButton';
 import LoadingScreen from '@components/Common/Loading/LoadingScreen';
 import { YStack } from 'tamagui';
 import RefreshControl from '@components/Common/Gui/RefreshControl';
+import { usePostScreen } from '@components/Post/hooks/usePostScreen';
 
 interface IPostScreenContext {
   refresh?: () => void;
@@ -38,11 +25,6 @@ const PostScreenContext: React.Context<IPostScreenContext> =
 export const usePostScreenContext = (): IPostScreenContext =>
   React.useContext(PostScreenContext);
 
-interface IProps {
-  navigation: NativeStackNavigationProp<any>;
-  route: any;
-}
-
 const renderItem = ({
   item,
 }: ListRenderItemInfo<ICommentInfo>): React.JSX.Element => {
@@ -51,117 +33,20 @@ const renderItem = ({
 
 const keyExtractor = (item: ICommentInfo): string => item.commentId.toString();
 
-export default function PostScreen({
-  navigation,
-  route,
-}: IProps): React.JSX.Element {
-  const { postId, parentCommentId } = route.params;
+export default function PostScreen(): React.JSX.Element {
+  const {
+    isLoading,
+    isError,
+    isRefreshing,
+    refresh,
 
-  const [postCollapsed, setPostCollapsed] = useState<boolean>(
-    parentCommentId != null,
-  );
-  const postCounts = usePostCounts(postId);
-  const postCommentsInfo = usePostCommentsInfo(postId);
-  const postCommunityId = usePostCommunityId(postId);
+    postCommentsInfo,
+    postCommunityId,
+    postCollapsed,
+    setPostCollapsed,
 
-  const newCommentId = useNewCommentId();
-
-  const defaultSortType = useDefaultCommentSort();
-
-  const initialized = useRef(false);
-
-  const [sortType, setSortType] = useState<CommentSortType>(
-    defaultSortType ?? 'Top',
-  );
-
-  const flashListRef = useRef<FlashList<ICommentInfo>>();
-
-  const { isLoading, isError, isRefreshing, refresh } = useLoadData(
-    async () => {
-      await instance.getComments({
-        options: {
-          post_id: postId,
-          community_id: postCommunityId ?? undefined,
-          sort: sortType,
-          parent_id: parentCommentId ?? undefined,
-          ...(parentCommentId != null && {
-            max_depth: 10,
-          }),
-        },
-        ignoreDepth: parentCommentId != null,
-      });
-
-      if (parentCommentId != null) {
-        flashListRef.current?.scrollToEnd({ animated: true });
-      }
-
-      initialized.current = true;
-    },
-  );
-
-  useEffect(() => {
-    navigation.setOptions({
-      title: `${postCounts?.comments} ${stripEss(
-        postCounts?.comments,
-        'Comments',
-      )}`,
-    });
-  }, []);
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <CommentSortTypeContextMenuButton
-          sortType={sortType}
-          setSortType={setSortType}
-        />
-      ),
-    });
-
-    if (!initialized.current) return;
-
-    flashListRef.current?.scrollToOffset({ offset: 0, animated: true });
-
-    refresh(async () => {
-      await instance.getComments({
-        options: {
-          post_id: postId,
-          community_id: postCommunityId ?? undefined,
-          sort: sortType,
-        },
-      });
-    });
-  }, [sortType]);
-
-  useEffect(() => {
-    if (newCommentId == null) return;
-
-    const index = postCommentsInfo?.findIndex(
-      (c) => c.commentId === newCommentId,
-    );
-
-    if (index == null || index === -1) return;
-
-    flashListRef.current?.scrollToIndex({
-      index,
-      animated: true,
-      viewOffset: 80,
-    });
-
-    setNewCommentId(undefined);
-  }, [newCommentId, postCommentsInfo]);
-
-  const onRefresh = useCallback(() => {
-    refresh(async () => {
-      await instance.getComments({
-        options: {
-          post_id: postId,
-          community_id: postCommunityId ?? undefined,
-          sort: sortType,
-        },
-      });
-    });
-  }, []);
+    flashListRef,
+  } = usePostScreen();
 
   if (postCommunityId == null) {
     return <LoadingScreen />;
@@ -170,7 +55,7 @@ export default function PostScreen({
   return (
     <PostScreenContext.Provider
       value={{
-        refresh: onRefresh,
+        refresh,
         postCollapsed,
         setPostCollapsed,
       }}
@@ -190,7 +75,7 @@ export default function PostScreen({
             />
           }
           refreshControl={
-            <RefreshControl onRefresh={onRefresh} refreshing={isRefreshing} />
+            <RefreshControl onRefresh={refresh} refreshing={isRefreshing} />
           }
           // @ts-expect-error this is valid
           ref={flashListRef}
