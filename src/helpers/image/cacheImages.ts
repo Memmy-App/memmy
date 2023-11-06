d9080454da382a2d43f2ebc0b5f55a0468580df3
Gavin <me@gkasdorf.com>
2023-11-05 21:30:10 -0800

    add viewerimage to post content

--- src/helpers/image/cacheImages.ts
+++ src/helpers/image/cacheImages.ts
@@ -1,6 +1,5 @@
 import { getLinkType } from '@helpers/links/getLinkType';
 import { Image } from 'expo-image';
-import { PostView } from 'lemmy-js-client';
 
 export const cacheImages = async (links: string[]): Promise<void> => {
   const imageLinks: string[] = [];
@@ -8,20 +7,6 @@ export const cacheImages = async (links: string[]): Promise<void> => {
   for (const link of links) {
     if (getLinkType(link) === 'image') {
       imageLinks.push(link);
-    }
-  }
-
-  Image.prefetch(imageLinks);
-};
-
-export const cacheImagesFromPosts = async (
-  posts: PostView[],
-): Promise<void> => {
-  const imageLinks: string[] = [];
-
-  for (const post of posts) {
-    if (post.post.url != null && getLinkType(post.post.url) === 'image') {
-      imageLinks.push(post.post.url);
     }
   }
 
